370cd267cfd7e981386e7146e6f997902b7fc765
ouss1002 <oboudjeniba@gmail.com>
2021-01-14 23:58:53 +0100

    Remove comment

--- preload.js
+++ preload.js
@@ -1,5 +1,5 @@
 // overwrite the `languages` property to use a custom getter
-// overwrite the `languages` property t
+// overwrite the `languages` property 
 Object.defineProperty(navigator, "languages", {
     get: function() {
         return ["en-US", "en"];
